b5b6dd488132adb65697f5709ef5d60b0df621fd
James Saunders <james@businessoptics.biz>
2020-03-08 21:52:38 +0200

    Update version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='fn_graph',
-    version='0.3.0',
+    version='0.4.0',
     description='Manage, maintain and reuse complex function graphs without the hassle.',
     python_requires='==3.*,>=3.7.0',
     project_urls={
